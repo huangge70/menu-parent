34c6914c113f7744369d37b2bcc5938e59b41837
huangge <17801093123@163.com>
2019-01-03 18:15:41 +0800

    Initial commit

---
+++ menu-service/src/main/java/com/bjsxt/dubbo/service/MenuDubboService.java
@@ -0,0 +1,11 @@
+package com.bjsxt.dubbo.service;
+
+import java.util.List;
+
+import com.bjsxt.pojo.Menu;
+
+public interface MenuDubboService {
+	List<Menu> selAll();
+	int updMenu(Menu menu);
+	Menu selById(int id);
+}
